bfa299a62699c9a765191da1f5152307a4f35390
Murray Christopherson <murraychristopherson@gmail.com>
2025-04-01 22:58:31 -0700

    starting work on the user page

--- app/routes/user.tsx
+++ app/routes/user.tsx
@@ -1,11 +1,86 @@
+import { LoaderFunction, MetaFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import {
+  DehydratedState,
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+  useQuery,
+} from '@tanstack/react-query';
 import React from 'react';
 
+import { getUserDetails } from '~/api/http/auth.http';
+import { getSessionId } from '~/api/sessionid.lib';
+import { useConfig } from '~/hooks/use-config';
+
+export const meta: MetaFunction<typeof loader> = () => {
+  return [
+    { title: 'Henhouse Server - Search' },
+    { name: 'description', content: 'Welcome to Henhouse!' },
+  ];
+};
+
+interface LoaderData {
+  dehydratedState: DehydratedState;
+}
+
+export const loader: LoaderFunction = async ({
+  request,
+}): Promise<LoaderData> => {
+  const cookieHeader = request.headers.get('Cookie');
+  let sessionId: string | null;
+  if (cookieHeader !== null) {
+    sessionId = getSessionId(cookieHeader);
+  } else {
+    sessionId = null;
+  }
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['user'],
+    queryFn: () => getUserDetails(process.env.API_HOST as string, sessionId),
+  });
+
+  return {
+    dehydratedState: dehydrate(queryClient),
+  };
+};
+
 const View: React.FC = () => {
-  return <div>User</div>;
+  const { data: configService } = useConfig();
+
+  const { data: user } = useQuery({
+    queryKey: ['user'],
+    queryFn: () => {
+      if (configService === undefined) {
+        throw new Error('configSerive undefined');
+      }
+      const host = configService.get<string>('API_HOST') as string;
+      return getUserDetails(host, null);
+    },
+    enabled: configService !== undefined,
+  });
+
+  if (user !== undefined) {
+    return (
+      <div>
+        User {user.username} ({user.email})
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 };
 
 const Index: React.FC = () => {
-  return <View />;
+  const { dehydratedState } = useLoaderData<LoaderData>();
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <View />
+    </HydrationBoundary>
+  );
 };
 
 export default Index;
